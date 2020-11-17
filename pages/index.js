d6fb80b1e2eea02bd54af570d6539136c0551d3b
kaelanbelaga <33121551+kaelanbelaga@users.noreply.github.com>
2020-11-17 18:04:01 +0100

    adds lazy hydration

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,30 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
+import LazyHydrate from 'react-lazy-hydration'
 
 import styles from '../styles/Home.module.css'
 
-// const Test = dynamic(() => import('../components/test'))
-// const Test2 = dynamic(() => import('../components/test2'))
-// const Test3 = dynamic(() => import('../components/test3'))
-// const Test4 = dynamic(() => import('../components/test4'))
-// const Test5 = dynamic(() => import('../components/test5'))
-// const Test6 = dynamic(() => import('../components/test6'))
-// const Test7 = dynamic(() => import('../components/test7'))
-// const Test8 = dynamic(() => import('../components/test8'))
-// const Test9 = dynamic(() => import('../components/test9'))
-// const Test10 = dynamic(() => import('../components/test10'))
-// const Test11 = dynamic(() => import('../components/test11'))
-// const Test12 = dynamic(() => import('../components/test12'))
-// const Test13 = dynamic(() => import('../components/test13'))
-// const Test14 = dynamic(() => import('../components/test14'))
-// const Test15 = dynamic(() => import('../components/test15'))
-// const Test16 = dynamic(() => import('../components/test16'))
-// const Test17 = dynamic(() => import('../components/test17'))
-// const Test18 = dynamic(() => import('../components/test18'))
-// const Test19 = dynamic(() => import('../components/test19'))
-// const Test20 = dynamic(() => import('../components/test20'))
+const Test = dynamic(() => import('../components/test'))
+const Test2 = dynamic(() => import('../components/test2'))
+const Test3 = dynamic(() => import('../components/test3'))
+const Test4 = dynamic(() => import('../components/test4'))
+const Test5 = dynamic(() => import('../components/test5'))
+const Test6 = dynamic(() => import('../components/test6'))
+const Test7 = dynamic(() => import('../components/test7'))
+const Test8 = dynamic(() => import('../components/test8'))
+const Test9 = dynamic(() => import('../components/test9'))
+const Test10 = dynamic(() => import('../components/test10'))
+const Test11 = dynamic(() => import('../components/test11'))
+const Test12 = dynamic(() => import('../components/test12'))
+const Test13 = dynamic(() => import('../components/test13'))
+const Test14 = dynamic(() => import('../components/test14'))
+const Test15 = dynamic(() => import('../components/test15'))
+const Test16 = dynamic(() => import('../components/test16'))
+const Test17 = dynamic(() => import('../components/test17'))
+const Test18 = dynamic(() => import('../components/test18'))
+const Test19 = dynamic(() => import('../components/test19'))
+const Test20 = dynamic(() => import('../components/test20'))
 
-import Test from '../components/test'
-import Test2 from '../components/test2'
-import Test3 from '../components/test3'
-import Test4 from '../components/test4'
-import Test5 from '../components/test5'
-import Test6 from '../components/test6'
-import Test7 from '../components/test7'
-import Test8 from '../components/test8'
-import Test9 from '../components/test9'
-import Test10 from '../components/test10'
-import Test11 from '../components/test11'
-import Test12 from '../components/test12'
-import Test13 from '../components/test13'
-import Test14 from '../components/test14'
-import Test15 from '../components/test15'
-import Test16 from '../components/test16'
-import Test17 from '../components/test17'
-import Test18 from '../components/test18'
-import Test19 from '../components/test19'
-import Test20 from '../components/test20'
 
 
 export default function Home() {
@@ -53,28 +34,33 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
+      <LazyHydrate ssrOnly>
+        <Test />
+        <Test2 />
+        <Test3 />
+        <Test4 />
+        <Test5 />
+        <Test6 />
+        <Test7 />
+        <Test8 />
+        <Test9 />
+        <Test10 />
+      </LazyHydrate>
 
-      <Test />
-      <Test2 />
-      <Test3 />
-      <Test4 />
-      <Test5 />
-      <Test6 />
-      <Test7 />
-      <Test8 />
-      <Test9 />
-      <Test10 />
-      <Test11 />
-      <Test12 />
-      <Test13 />
-      <Test14 />
-      <Test15 />
-      <Test16 />
-      <Test17 />
-      <Test18 />
-      <Test19 />
-      <Test20 />
-
+      <LazyHydrate on="click">
+        <Test11 />
+        <Test12 />
+        <Test13 />
+        <Test14 />
+        <Test15 />
+        <Test16 />
+        <Test17 />
+        <Test18 />
+        <Test19 />
+        <Test20 />
+      </LazyHydrate>
+      
     </div>
   )
 }
